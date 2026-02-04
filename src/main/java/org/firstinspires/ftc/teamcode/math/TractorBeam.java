138f48f612acf216e6ad8a2bccfc7725a34f3b03
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2026-02-03 19:43:46 -0600

    make maximum turret angle configurable

--- src/main/java/org/firstinspires/ftc/teamcode/math/TractorBeam.java
+++ src/main/java/org/firstinspires/ftc/teamcode/math/TractorBeam.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode.math;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.pedropathing.geometry.Pose;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.robot.Alliance;
 import org.firstinspires.ftc.teamcode.robot.Robot;
 
+@Config
 public class TractorBeam {
+    public static double maximumAngleDegrees = 145;
+
     public static void aimTurret(Robot robot, Alliance alliance) {
         Pose currentPose = robot.drivetrain.getPose();
         double targetAngleRadians = Math.atan2(alliance.goal.getY() - currentPose.getY(), alliance.goal.getX() - currentPose.getX());
@@ -14,7 +18,7 @@ public class TractorBeam {
 
         double turretTargetDegrees = Math.toDegrees(turretTargetRadians);
 
-        if (turretTargetDegrees > 145) {
+        if (turretTargetDegrees > maximumAngleDegrees) {
             turretTargetDegrees = turretTargetDegrees - 360;
         }
 
